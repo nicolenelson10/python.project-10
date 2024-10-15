43e9d2f7146eb60648b0733d86f54d9edb219f4e
nicolenelson10 <nicolenelson2028@cs.sjcetpalai.ac.in>
2024-10-15 14:11:04 +0530

    Add files via upload

---
+++ nicole.py
@@ -0,0 +1,9 @@
+'''
+python program to check whether the given number is positive
+author name:Nicole Nelson
+'''
+number=int(input("enter a number:"))
+if number>0:
+    print("the given number:",number,"is positive")
+else:
+    print("the given number:",number,"is negative")
