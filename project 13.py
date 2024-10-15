5861f5d04f6a5ffd7dcaf1d9da641f822426ce40
nicolenelson10 <nicolenelson2028@cs.sjcetpalai.ac.in>
2024-10-15 14:45:16 +0530

    Add files via upload

---
+++ project 13.py
@@ -0,0 +1,10 @@
+'''
+author name:Nicole Nelson
+to check if a number is divisible by 5.
+'''
+number=int(input("enter the number:"))
+if (number%5==0):
+    print("divisible by 5")
+else:
+    print("not divible by 5")
+
