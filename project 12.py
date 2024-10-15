1201c299ee604a25da23e2da1f3e309fb9f2e5ec
nicolenelson10 <nicolenelson2028@cs.sjcetpalai.ac.in>
2024-10-15 14:38:13 +0530

    Add files via upload

---
+++ project 12.py
@@ -0,0 +1,12 @@
+'''
+author name:Nicole Nelson
+to check if a person is eligible for driving license.
+'''
+age=int(input("enter the age:"))
+if age>=16:
+    print("eligible for driving license:",age)
+else:
+    print("not eligible for driving license:",age)
+
+
+
